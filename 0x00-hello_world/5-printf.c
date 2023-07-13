887722bcae304e29f97af5beab4bce4588df16f7
Austinet <austinetsplendour@gmail.com>
2023-07-13 15:17:58 -0700

    update 5-printf file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
- /**
-  * main - Entry point
-  *
-  * Description: A C program to print a string
-  *using printf
-  * Return: 0 as success
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

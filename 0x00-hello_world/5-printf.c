a79a1065043b9ed4a0756d1e115ea93bba8bfd7c
Austinet <austinetsplendour@gmail.com>
2023-07-13 13:02:00 -0700

    Add 5-printf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ /*
+  *function main: main function
+  *
+  *description: A C program to print a string
+  *using printf
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

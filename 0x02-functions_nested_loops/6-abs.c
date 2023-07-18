52be940ca96732d6bf6e30f7804605cc391d0c39
Austinet <austinetsplendour@gmail.com>
2023-07-18 15:09:55 -0700

    Add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @num : parameter to compute the absolute value
+ * Return: absolute value
+ */
+int _abs(int num)
+{
+if (num >= 0)
+{
+return (num);
+}
+else
+{
+return (-num);
+}
+}

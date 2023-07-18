652e33b91f171d0518b044e80b4c1c5719574e3d
Austinet <austinetsplendour@gmail.com>
2023-07-18 14:25:29 -0700

    Add 4-isalpha.c file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - checks if a character is an alphabet or not
+ * @c : parameter to check if lower case or not
+ * Return: 1 if alphabet, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 65 || c <= 90))
+return (1);
+else
+return (0);
+}

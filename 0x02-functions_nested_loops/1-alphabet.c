272383c63afd5ace58ca69de889434a2e1f70dbd
Austinet <austinetsplendour@gmail.com>
2023-07-18 12:13:58 -0700

    Add 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * print_alphabet - print alphabets in lower case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+_putchar(lowerCase);
+lowerCase++;
+}
+_putchar('\n');
+}

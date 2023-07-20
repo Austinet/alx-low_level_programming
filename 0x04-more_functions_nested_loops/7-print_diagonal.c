07724aad768bd83ea4a2dc7735ae15dbaae54679
Austinet <austinetsplendour@gmail.com>
2023-07-20 12:21:44 -0700

    update 7-print_diagonal.c file

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,17 @@ void print_diagonal(int n)
 {
 int space = 0;
 int j;
+int k = n;
 while (n > 0)
 {
 _putchar('\\');
+if (space != (k - 1))
+{
 _putchar('\n');
 for (j = 0; j <= space; j++)
 {
 _putchar(' ');
+}
 }
 space++;
 n--;

6bcbca42b43804f735cbc49684290b167e252181
Austinet <austinetsplendour@gmail.com>
2023-07-22 08:04:36 -0700

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+   * add - adds two numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: returns the addition of two numbers
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}

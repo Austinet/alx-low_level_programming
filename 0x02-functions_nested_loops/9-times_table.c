11d2e551074c5f5814e70ec3b19edf6df78c82ea
Austinet <austinetsplendour@gmail.com>
2023-07-22 09:20:20 -0700

    update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,29 @@ for (i = 0; i <= 9; i++)
 for (j = 0; j <= 9; j++)
 {
 product = i * j;
+if (j > 0)
+{
 if (product > 9)
 {
+_putchar(' ');
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
 }
 else
 {
+_putchar(' ');
+_putchar(' ');
 _putchar(product + '0');
 }
-if (j != 9)
+}
+else
+{
+_putchar(product + '0');
+}
+
+if (j < 9)
 {
 _putchar(',');
-_putchar(' ');
 }
 }
 _putchar('\n');

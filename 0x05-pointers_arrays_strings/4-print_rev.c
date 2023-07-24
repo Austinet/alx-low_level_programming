3098182a6bd1fd80f10d050afc6f3103e8b8f634
Austinet <austinetsplendour@gmail.com>
2023-07-24 13:38:02 -0700

    update 4-priny_rev.c file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,14 @@ void print_rev(char *s)
 {
 int i = 0;
 
+/*calculates the string length*/
 while (s[i] != '\0')
 {
 i++;
 }
+/*Removes the \0 character */
+i--;
+/*Prints the string in reverse*/
 for (; i >= 0; i--)
 {
 _putchar(s[i]);

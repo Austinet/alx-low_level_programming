794cc90b0e5492b617db31605ad5e80ffbe7688b
Austinet <austinetsplendour@gmail.com>
2023-08-02 05:11:13 -0700

    Add 0-puts_recursion.c and main.h files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _puts_recursion - prints a string
+ * @s: the string to print
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+/* *s points to the address of the first element*/
+if (*s != '\0')
+{
+_putchar(*s);
+_puts_recursion(++s); /* point to the address of the next element*/
+}
+_putchar('\n');
+}

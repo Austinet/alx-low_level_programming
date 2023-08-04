ca392a069d70b66c2410f7e8d56e8f5e7bd2f890
Austinet <austinetsplendour@gmail.com>
2023-08-04 14:20:31 -0700

    Add 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints the name of a program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: name of the program
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}

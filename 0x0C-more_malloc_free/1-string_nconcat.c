a7f08a9437d3864a275964b4ccfb7ad4e4b0163e
Austinet <austinetsplendour@gmail.com>
2023-08-10 16:50:36 -0700

    update 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,9 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *conc;
-int k, l;
-int i = 0;
-int j = 0;
+unsigned int k, l;
+unsigned int i = 0;
+unsigned int j = 0;
 
 if (s1 == NULL)
 s1 = "";

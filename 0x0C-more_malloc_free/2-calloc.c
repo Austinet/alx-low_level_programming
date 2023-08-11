c037017c62464ef618fc8a7910907bda0e3d08e3
Austinet <austinetsplendour@gmail.com>
2023-08-10 18:08:47 -0700

    update calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,21 +9,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *arr;
+int *arr;
 unsigned int i;
 
-if (size <= 0 || nmemb <= 0)
+if (nmemb <= 0 || size <= 0)
 return (NULL);
 
-arr = malloc(size * nmemb);
+arr = malloc(nmemb * size);
 
 if (arr == NULL)
 return (NULL);
 
 for (i = 0; i < nmemb; i++)
-{
 arr[i] = 0;
-}
 
 return (arr);
 }

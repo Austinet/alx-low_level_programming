291edcf879fb3cf524f2fc15270cf8b9244e906b
Austinet <austinetsplendour@gmail.com>
2023-08-24 17:43:13 -0700

    add 4-free_list.c file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list.
+ * @head: the beginning of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+list_t *temp;
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
